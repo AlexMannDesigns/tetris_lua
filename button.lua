b119d3526201372a569f14a680f3dcf068e62425
AlexMannDesigns <alex.mann1989@gmail.com>
2022-02-03 13:57:56 +0200

    added start game menu

---
+++ button.lua
@@ -0,0 +1,55 @@
+local love = require "love"
+local lg = love.graphics
+-- Button function is effectively a class that will aways return an object, populated with args passed to it
+function Button(text, func, func_param, width, height)
+	return {
+		width = width or 100,
+		height = height or 100,
+		func = func or function () print("this function has no value")	end,
+		func_param = func_param,
+		text = text or "no text",
+		button_x = 0,
+		button_y = 0,
+		text_x = 0,
+		text_y = 0,
+
+		checkPressed = function(self, mouse_x, mouse_y) -- check click has happened within the boundaries of the button
+			if (mouse_x >= self.button_x) and (mouse_x <= self.button_x + self.width) then
+			 	if (mouse_y >= self.button_y) and (mouse_y <= self.button_x + self.height) then
+					if self.func_param then
+						self.func(self.func_param) --if function has params, call with args
+					else
+						self.func()
+					end
+				end
+			end
+		end,
+
+		draw = function (self, button_x, button_y, text_x, text_y)
+			self.button_x = button_x or self.button_x
+			self.button_y = button_y or self.button_y
+
+			if text_x then
+				self.text_x = text_x + self.button_x -- text must be written relative to position of button
+			else
+				self.text_x = self.button_x
+			end
+
+			if text_y then
+				self.text_y = text_y + self.button_y
+			else
+				self.text_y = self.button_y
+			end
+
+			lg.setColor(0.6, 0.6, 0.6)
+			lg.rectangle("fill", self.button_x, self.button_y, self.width, self.height)
+
+			lg.setColor(0, 0, 0)
+			lg.print(self.text, self.text_x, self.text_y)
+
+			lg.setColor(1, 1, 1) -- always falls back to white by default
+		end,
+	}
+end
+
+return Button
--- main.lua
+++ main.lua
@@ -1,158 +1,171 @@
 -- TO DO
--- game over screen, 'push any key to restart'
+-- game over/game start screen, 'push any key to restart'
 
-function love.load() --called once at begin of game
-	love.graphics.setBackgroundColor(255,255,255)
+local love = require "love" -- silences linter warnings for love functions
+local lg = love.graphics
+local button = require "Button" -- importing our button function as a 'class'
 
+-- playing with states...
+local game = {
+	state = {
+		menu = true,
+		running = false,
+		ended = false
+	},
+}
+
+local buttons = {
+	menu_state = {}, -- this obj will contain all the buttons to be rendered when gamestate=menu
+}
+
+function startNewGame()
+	game.state["menu"] = false
+	game.state["running"] = true
 	-- key for table: { {piece type {rotations}}, {piece type {rotations}}, {piece type {rotations}} }
 	pieceStructures = {
-		{
 			{
-				{' ', ' ', ' ', ' '},
-				{'i', 'i', 'i', 'i'},
-				{' ', ' ', ' ', ' '},
-				{' ', ' ', ' ', ' '},
+				{
+					{' ', ' ', ' ', ' '},
+					{'i', 'i', 'i', 'i'},
+					{' ', ' ', ' ', ' '},
+					{' ', ' ', ' ', ' '},
+				},
+				{
+					{' ', 'i', ' ', ' '},
+					{' ', 'i', ' ', ' '},
+					{' ', 'i', ' ', ' '},
+					{' ', 'i', ' ', ' '},
+				},
 			},
 			{
-				{' ', 'i', ' ', ' '},
-				{' ', 'i', ' ', ' '},
-				{' ', 'i', ' ', ' '},
-				{' ', 'i', ' ', ' '},
+				{
+					{' ', ' ', ' ', ' '},
+					{' ', 'o', 'o', ' '},
+					{' ', 'o', 'o', ' '},
+					{' ', ' ', ' ', ' '},
+				},
 			},
-		},
-		{
 			{
-				{' ', ' ', ' ', ' '},
-				{' ', 'o', 'o', ' '},
-				{' ', 'o', 'o', ' '},
-				{' ', ' ', ' ', ' '},
+				{
+	    	        {' ', ' ', ' ', ' '},
+	    	        {'j', 'j', 'j', ' '},
+	    	        {' ', ' ', 'j', ' '},
+	    	        {' ', ' ', ' ', ' '},
+	    	    },
+	    	    {
+	    	        {' ', 'j', ' ', ' '},
+	    	        {' ', 'j', ' ', ' '},
+	    	        {'j', 'j', ' ', ' '},
+	    	        {' ', ' ', ' ', ' '},
+	    	    },
+	    	    {
+	    	        {'j', ' ', ' ', ' '},
+	    	        {'j', 'j', 'j', ' '},
+	    	        {' ', ' ', ' ', ' '},
+	    	        {' ', ' ', ' ', ' '},
+	    	    },
+	    	    {
+	    	        {' ', 'j', 'j', ' '},
+	    	        {' ', 'j', ' ', ' '},
+	    	        {' ', 'j', ' ', ' '},
+	    	        {' ', ' ', ' ', ' '},
+	    	    },
 			},
-		},
-		{
 			{
-	            {' ', ' ', ' ', ' '},
-	            {'j', 'j', 'j', ' '},
-	            {' ', ' ', 'j', ' '},
-	            {' ', ' ', ' ', ' '},
-	        },
-	        {
-	            {' ', 'j', ' ', ' '},
-	            {' ', 'j', ' ', ' '},
-	            {'j', 'j', ' ', ' '},
-	            {' ', ' ', ' ', ' '},
-	        },
-	        {
-	            {'j', ' ', ' ', ' '},
-	            {'j', 'j', 'j', ' '},
-	            {' ', ' ', ' ', ' '},
-	            {' ', ' ', ' ', ' '},
-	        },
-	        {
-	            {' ', 'j', 'j', ' '},
-	            {' ', 'j', ' ', ' '},
-	            {' ', 'j', ' ', ' '},
-	            {' ', ' ', ' ', ' '},
-	        },
-		},
-		{
-	        {
-	            {' ', ' ', ' ', ' '},
-	            {'l', 'l', 'l', ' '},
-	            {'l', ' ', ' ', ' '},
-	            {' ', ' ', ' ', ' '},
-	        },
-	        {
-	            {' ', 'l', ' ', ' '},
-	            {' ', 'l', ' ', ' '},
-	            {' ', 'l', 'l', ' '},
-	            {' ', ' ', ' ', ' '},
-	        },
-	        {
-	            {' ', ' ', 'l', ' '},
-	            {'l', 'l', 'l', ' '},
-	            {' ', ' ', ' ', ' '},
-	            {' ', ' ', ' ', ' '},
-	        },
-	        {
-	            {'l', 'l', ' ', ' '},
-	            {' ', 'l', ' ', ' '},
-	            {' ', 'l', ' ', ' '},
-	            {' ', ' ', ' ', ' '},
-	        },
-	    },
-		{
-	        {
-	            {' ', ' ', ' ', ' '},
-	            {'t', 't', 't', ' '},
-	            {' ', 't', ' ', ' '},
-	            {' ', ' ', ' ', ' '},
-	        },
-	        {
-	            {' ', 't', ' ', ' '},
-	            {' ', 't', 't', ' '},
-	            {' ', 't', ' ', ' '},
-	            {' ', ' ', ' ', ' '},
-	        },
-	        {
-	            {' ', 't', ' ', ' '},
-	            {'t', 't', 't', ' '},
-	            {' ', ' ', ' ', ' '},
-	            {' ', ' ', ' ', ' '},
-	        },
-	        {
-	            {' ', 't', ' ', ' '},
-	            {'t', 't', ' ', ' '},
-	            {' ', 't', ' ', ' '},
-	            {' ', ' ', ' ', ' '},
-	        },
-	    },
-		{
-	        {
-	            {' ', ' ', ' ', ' '},
-	            {' ', 's', 's', ' '},
-	            {'s', 's', ' ', ' '},
-	            {' ', ' ', ' ', ' '},
-	        },
-	        {
-	            {'s', ' ', ' ', ' '},
-	            {'s', 's', ' ', ' '},
-	            {' ', 's', ' ', ' '},
-	            {' ', ' ', ' ', ' '},
-	        },
-	    },
-	    {
-	        {
-	            {' ', ' ', ' ', ' '},
-	            {'z', 'z', ' ', ' '},
-	            {' ', 'z', 'z', ' '},
-	            {' ', ' ', ' ', ' '},
-	        },
-	        {
-	            {' ', 'z', ' ', ' '},
-	            {'z', 'z', ' ', ' '},
-	            {'z', ' ', ' ', ' '},
-	            {' ', ' ', ' ', ' '},
-	        },
-	    },
+	    	    {
+	    	        {' ', ' ', ' ', ' '},
+	    	        {'l', 'l', 'l', ' '},
+	    	        {'l', ' ', ' ', ' '},
+	    	        {' ', ' ', ' ', ' '},
+	    	    },
+	    	    {
+	    	        {' ', 'l', ' ', ' '},
+	    	        {' ', 'l', ' ', ' '},
+	    	        {' ', 'l', 'l', ' '},
+	    	        {' ', ' ', ' ', ' '},
+	    	    },
+	    	    {
+	    	        {' ', ' ', 'l', ' '},
+	    	        {'l', 'l', 'l', ' '},
+	    	        {' ', ' ', ' ', ' '},
+	    	        {' ', ' ', ' ', ' '},
+	    	    },
+	    	    {
+	    	        {'l', 'l', ' ', ' '},
+	    	        {' ', 'l', ' ', ' '},
+	    	        {' ', 'l', ' ', ' '},
+	    	        {' ', ' ', ' ', ' '},
+	    	    },
+	    	},
+			{
+	    	    {
+	    	        {' ', ' ', ' ', ' '},
+	    	        {'t', 't', 't', ' '},
+	    	        {' ', 't', ' ', ' '},
+	    	        {' ', ' ', ' ', ' '},
+	    	    },
+	    	    {
+	    	        {' ', 't', ' ', ' '},
+	    	        {' ', 't', 't', ' '},
+	    	        {' ', 't', ' ', ' '},
+	    	        {' ', ' ', ' ', ' '},
+	    	    },
+	    	    {
+	    	        {' ', 't', ' ', ' '},
+	    	        {'t', 't', 't', ' '},
+	    	        {' ', ' ', ' ', ' '},
+	    	        {' ', ' ', ' ', ' '},
+	    	    },
+	    	    {
+	    	        {' ', 't', ' ', ' '},
+	    	        {'t', 't', ' ', ' '},
+	    	        {' ', 't', ' ', ' '},
+	    	        {' ', ' ', ' ', ' '},
+	    	    },
+	    	},
+			{
+	    	    {
+	    	        {' ', ' ', ' ', ' '},
+	    	        {' ', 's', 's', ' '},
+	    	        {'s', 's', ' ', ' '},
+	    	        {' ', ' ', ' ', ' '},
+	    	    },
+	    	    {
+	    	        {'s', ' ', ' ', ' '},
+	    	        {'s', 's', ' ', ' '},
+	    	        {' ', 's', ' ', ' '},
+	    	        {' ', ' ', ' ', ' '},
+	    	    },
+	    	},
+	    	{
+	    	    {
+	    	        {' ', ' ', ' ', ' '},
+	    	        {'z', 'z', ' ', ' '},
+	    	        {' ', 'z', 'z', ' '},
+	    	        {' ', ' ', ' ', ' '},
+	    	    },
+	    	    {
+	    	        {' ', 'z', ' ', ' '},
+	    	        {'z', 'z', ' ', ' '},
+	    	        {'z', ' ', ' ', ' '},
+	    	        {' ', ' ', ' ', ' '},
+	    	    },
+	    	},
 	}
 	--tetr loops variable
 	pieceXcount = 4
 	pieceYcount = 4
 	--varibable to set the fall speed of the tetrimino
 	timerLimit = 0.5
-
 	gridXcount = 10 --arena width
 	gridYcount = 18 --arena height
-
 	lineCount = 0 --tracks number of complete lines
 	level = 0
-
 	function canPieceMove(testX, testY, testRotation) --function returns true or false after checking position in arena
 		for y = 1, pieceYcount do --check tetrimino 4x4 grid
 			for x = 1, pieceXcount do
 				local testBlockX = testX + x
 				local testBlockY = testY + y
-
 				if pieceStructures[pieceType][testRotation][y][x] ~= ' ' and (
 					testBlockX < 1
 					or testBlockX > gridXcount
@@ -163,10 +176,8 @@ function love.load() --called once at begin of game
 				end
 			end
 		end
-
 		return true
 	end
-
 	function newSequence() --generates a table of randomly ordered tetriminos
 		sequence = {}
 		for pieceTypeIndex = 1, #pieceStructures do -- loop through all tetriminos
@@ -178,7 +189,6 @@ function love.load() --called once at begin of game
 			)
 		end
 	end
-
 	function newPiece()
 		--below coords are fed into the draw block function to set location of tetrimino
 		pieceX = 3
@@ -186,12 +196,10 @@ function love.load() --called once at begin of game
 		--below coords refer to tetrimino shape and rotation in above table
 		pieceRotation = 1
 		pieceType = table.remove(sequence) --tetrimino taken from end of table
-
 		if #sequence == 0 then --new table generated when sequence length is 0
 			newSequence()
 		end
 	end
-
 	function reset()
 		inert = {} --creates a 2d array, reperesenting the arena
 		for y = 1, gridYcount do
@@ -207,171 +215,202 @@ function love.load() --called once at begin of game
 		lineCount = 0
 		level = 0
 	end
-
 	reset()
 end
 
-function love.update(dt)
-	timer = timer + dt
-	if timer >= timerLimit then
-		timer = 0
+function love.load() --called once at beginning of game
+	love.window.setTitle("TETRIS")
+	lg.setBackgroundColor(255,255,255)
 
-		local testY = pieceY + 1
-		if canPieceMove(pieceX, testY, pieceRotation) then	--if the piece can move down, move it
-			pieceY = testY
-		else	-- if it can't move (i.e. bottom of arena or another tetrimino below)
-			-- add tetrimino to inert by looping indicies of 2d array
-			for y = 1, pieceYcount do
-				for x = 1, pieceXcount do
-					local block =
-						pieceStructures[pieceType][pieceRotation][y][x] -- block matches char in tetrimino
-					if block ~= ' ' then
-						inert[pieceY + y][pieceX + x] = block
-					end
-				end
-			end
-			for y = 1, gridYcount do
-				local complete = true
-				for x = 1, gridXcount do
-					if inert[y][x] == ' ' then
-						complete = false
-						break
-					end
-				end
-				if complete then --we have found a completed row
-					for removeY = y, 2, -1 do --subtract 1 until we get to 2. The top row does not need to be checked
-						for removeX = 1, gridXcount do
-							inert[removeY][removeX] = inert[removeY - 1][removeX] --move blocks above line down by one
+	if game.state["menu"] then
+		buttons.menu_state.exit_game = button("Exit", love.event.quit, nil, 120, 40) -- event.quit function closes love
+		buttons.menu_state.play_game = button("Play Game", startNewGame, nil, 120, 40)
+	end
+end
+
+function love.update(dt)
+	if game.state["running"] then
+		timer = timer + dt
+		if timer >= timerLimit then
+			timer = 0
+
+			local testY = pieceY + 1
+			if canPieceMove(pieceX, testY, pieceRotation) then	--if the piece can move down, move it
+				pieceY = testY
+			else	-- if it can't move (i.e. bottom of arena or another tetrimino below)
+				-- add tetrimino to inert by looping indicies of 2d array
+				for y = 1, pieceYcount do
+					for x = 1, pieceXcount do
+						local block =
+							pieceStructures[pieceType][pieceRotation][y][x] -- block matches char in tetrimino
+						if block ~= ' ' then
+							inert[pieceY + y][pieceX + x] = block
 						end
 					end
-
-					for removeX = 1, gridXcount do --sets top row of arena to blank
-						inert[1][removeX] = ' '
+				end
+				for y = 1, gridYcount do
+					local complete = true
+					for x = 1, gridXcount do
+						if inert[y][x] == ' ' then
+							complete = false
+							break
+						end
 					end
+					if complete then --we have found a completed row
+						for removeY = y, 2, -1 do --subtract 1 until we get to 2. The top row does not need to be checked
+							for removeX = 1, gridXcount do
+								inert[removeY][removeX] = inert[removeY - 1][removeX] --move blocks above line down by one
+							end
+						end
 
-					lineCount = lineCount + 1 --increment line counter for each complete line
-					if lineCount % 5 == 0 then
-						level = level + 1
-						timerLimit = timerLimit * 0.9
+						for removeX = 1, gridXcount do --sets top row of arena to blank
+							inert[1][removeX] = ' '
+						end
+
+						lineCount = lineCount + 1 --increment line counter for each complete line
+						if lineCount % 5 == 0 then
+							level = level + 1
+							timerLimit = timerLimit * 0.9
+						end
 					end
 				end
-			end
-			newPiece()
+				newPiece()
 
-			if not canPieceMove(pieceX, pieceY, pieceRotation) then --restart game if new piece cant be moved
-				reset()
+				if not canPieceMove(pieceX, pieceY, pieceRotation) then --restart game if new piece cant be moved
+					reset()
+				end
+			end
+		end
+	end
+end
+
+function love.mousepressed(x, y, click) -- detect mouse clicks
+	if not game.state["running"] then -- we only want mouse to work when game is not running
+		if click == 1 then
+			if game.state["menu"] then
+				for index in pairs(buttons.menu_state) do
+					buttons.menu_state[index]:checkPressed(x, y)
+				end
 			end
 		end
 	end
 end
 
 function love.keypressed(key) --rotate and move tetriminos
-	if key == 'x' or key == 'up' then --increment rotation by 1, unless end no rotations, then set to 1
-		local testRotation = pieceRotation + 1
-		if testRotation > #pieceStructures[pieceType] then
-			testRotation = 1
-		end
+	if game.state["running"] then
+		if key == 'x' or key == 'up' then --increment rotation by 1, unless end no rotations, then set to 1
+			local testRotation = pieceRotation + 1
+			if testRotation > #pieceStructures[pieceType] then
+				testRotation = 1
+			end
 
-		if canPieceMove(pieceX, pieceY, testRotation) then
-			pieceRotation = testRotation
-		end
+			if canPieceMove(pieceX, pieceY, testRotation) then
+				pieceRotation = testRotation
+			end
 
-	elseif key == 'z' then --decrement rotation by 1, unless it gets to zero
-		local testRotation = pieceRotation - 1
-		if testRotation < 1 then
-			testRotation = #pieceStructures[pieceType]
-		end
+		elseif key == 'z' then --decrement rotation by 1, unless it gets to zero
+			local testRotation = pieceRotation - 1
+			if testRotation < 1 then
+				testRotation = #pieceStructures[pieceType]
+			end
 
-		if canPieceMove(pieceX, pieceY, testRotation) then
-			pieceRotation = testRotation
-		end
+			if canPieceMove(pieceX, pieceY, testRotation) then
+				pieceRotation = testRotation
+			end
 
-	elseif key == 'left' then
-		local testX = pieceX - 1
+		elseif key == 'left' then
+			local testX = pieceX - 1
 
-		if canPieceMove(testX, pieceY, pieceRotation) then
-			pieceX = testX
-		end
+			if canPieceMove(testX, pieceY, pieceRotation) then
+				pieceX = testX
+			end
 
-	elseif key == 'right' then
-		local testX = pieceX + 1
+		elseif key == 'right' then
+			local testX = pieceX + 1
 
-		if canPieceMove(testX, pieceY, pieceRotation) then
-			pieceX = testX
-		end
+			if canPieceMove(testX, pieceY, pieceRotation) then
+				pieceX = testX
+			end
 
-	elseif key == 'c' or key == 'down' then
-		while canPieceMove(pieceX, pieceY + 1, pieceRotation) do
-			pieceY = pieceY + 1
-			timer = timerLimit
+		elseif key == 'c' or key == 'down' then
+			while canPieceMove(pieceX, pieceY + 1, pieceRotation) do
+				pieceY = pieceY + 1
+				timer = timerLimit
+			end
 		end
 	end
 end
 --function to draw arena
 function love.draw()
-	local function drawBlock(block, x, y) --function handles the drawing of each square in arena
-		local colors = {
-			[' '] = {.87, .87, .87},
-			i = {.47, .76, .94},
-			j = {.93, .91, .42},
-			l = {.49, .85, .76},
-			o = {.92, .69, .47},
-			s = {.83, .54, .93},
-			t = {.97, .58, .77},
-			z = {.66, .83, .46},
-			preview = {.75, .75, .75},
-		}
-		local color = colors[block] --checks value of spaces in arena
-		love.graphics.setColor(color) --uses table above to set space to color
+	if game.state["running"] then
+		local function drawBlock(block, x, y) --function handles the drawing of each square in arena
+			local colors = {
+				[' '] = {.87, .87, .87},
+				i = {.47, .76, .94},
+				j = {.93, .91, .42},
+				l = {.49, .85, .76},
+				o = {.92, .69, .47},
+				s = {.83, .54, .93},
+				t = {.97, .58, .77},
+				z = {.66, .83, .46},
+				preview = {.75, .75, .75},
+			}
+			local color = colors[block] --checks value of spaces in arena
+			lg.setColor(color) --uses table above to set space to color
 
-		local blockSize = 20 --size of squares
-		local blockDrawSize = blockSize - 1 --size of lines between squares
-		love.graphics.rectangle(
-			'fill',				 --type of rectangle
-			(x - 1) * blockSize, --position on x
-			(y - 1) * blockSize, --position on y
-			blockDrawSize, --width
-			blockDrawSize --height
-			--can add border radius here (x px, y px, and num of segments)
-		)
-	end
-
-	local offsetX = 2
-	local offsetY = 5
-
-	for y = 1, gridYcount do --these nested loops draw the arena
-		for x = 1, gridXcount do
-			drawBlock(inert[y][x], x + offsetX, y + offsetY)
+			local blockSize = 20 --size of squares
+			local blockDrawSize = blockSize - 1 --size of lines between squares
+			lg.rectangle(
+				'fill',				 --type of rectangle
+				(x - 1) * blockSize, --position on x
+				(y - 1) * blockSize, --position on y
+				blockDrawSize, --width
+				blockDrawSize --height
+				--can add border radius here (x px, y px, and num of segments)
+			)
 		end
-	end
 
-	for y = 1, pieceYcount do --these nested loops draw the tetrimino
-		for x = 1, pieceXcount do
-			local block = pieceStructures[pieceType][pieceRotation][y][x] --sets block based on above variables
-			if block ~= ' ' then --if the tetrimino location is not empty, set a color, otherwise move on
-				drawBlock(block, x + pieceX + offsetX, y + pieceY + offsetY)
-			end --ends if block
-		end --ends inner tetrimino draw loop
-	end --ends outer tetrimino draw loop
+		local offsetX = 2
+		local offsetY = 5
 
-	for y = 1, pieceYcount do
-		for x = 1, pieceXcount do
-			local block = pieceStructures[sequence[#sequence]][1][y][x] -- draw preview of next piece
-			if block ~= ' ' then
-				drawBlock('preview', x + 5, y + 1) -- position over the arena, color grey
+		for y = 1, gridYcount do --these nested loops draw the arena
+			for x = 1, gridXcount do
+				drawBlock(inert[y][x], x + offsetX, y + offsetY)
 			end
 		end
+
+		for y = 1, pieceYcount do --these nested loops draw the tetrimino
+			for x = 1, pieceXcount do
+				local block = pieceStructures[pieceType][pieceRotation][y][x] --sets block based on above variables
+				if block ~= ' ' then --if the tetrimino location is not empty, set a color, otherwise move on
+					drawBlock(block, x + pieceX + offsetX, y + pieceY + offsetY)
+				end --ends if block
+			end --ends inner tetrimino draw loop
+		end --ends outer tetrimino draw loop
+
+		for y = 1, pieceYcount do
+			for x = 1, pieceXcount do
+				local block = pieceStructures[sequence[#sequence]][1][y][x] -- draw preview of next piece
+				if block ~= ' ' then
+					drawBlock('preview', x + 5, y + 1) -- position over the arena, color grey
+				end
+			end
+		end
+		local str1 = "Lines: " .. lineCount
+		local str2 = "level: " .. level
+		local color1 = {.83, .54, .93}
+		local lineText = {color1, str1}
+		local levelText = {color1, str2}
+		font = lg.newImageFont("awesomefont.png",
+			" abcdefghijklmnopqrstuvwxyz" ..
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZ0" ..
+			"123456789.,!?-+/():;%&`'*#=[]\"")
+		lg.setFont(font)
+		lg.print(lineText, font, 300, 200)
+		lg.print(levelText, font, 300, 230)
+
+	elseif game.state["menu"] then
+		buttons.menu_state.play_game:draw(300, 100, 17, 10)
+		buttons.menu_state.exit_game:draw(300, 150, 17, 10)
 	end
-	local str1 = "Lines: " .. lineCount
-	local str2 = "level: " .. level
-	local color1 = {.83, .54, .93}
-	local lineText = {color1, str1}
-	local levelText = {color1, str2}
-	font = love.graphics.newImageFont("awesomefont.png",
-		" abcdefghijklmnopqrstuvwxyz" ..
-		"ABCDEFGHIJKLMNOPQRSTUVWXYZ0" ..
-		"123456789.,!?-+/():;%&`'*#=[]\"")
-	love.graphics.setFont(font)
-	love.graphics.print(lineText, font, 300, 200)
-	love.graphics.print(levelText, font, 300, 230)
 end
